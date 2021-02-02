057f3b6dc1f9013f286635a04c0ec396b064f2dc
wasipo <horiguchiko@gmail.com>
2021-02-02 23:29:23 +0900

    エンターキー押下で、タスクを追加する機能作成

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,21 @@
 import {Item} from './item';
 import $ from 'jquery';
 const elem = document.getElementById('output');
-// const aBook = new Item('はじめてのTypeScript',132415);
 
-const bookname : string = "若者のすべて";
-const price : number = 99999;
+//const j:JQueryStatic = $;
+const rootID:string = '#output';
+const inputID:string = '#outputTask';
+const max:number = 10;
 
-interface book {
-    title: string,
-    price: number,
-}
+$(inputID).on('keyup',function(e){    
+    if (e.key === "Enter"){
+        let childrenNumber:number = Number($(rootID).children().length);
+        childrenNumber++;
+        if(childrenNumber >= max) {
+            alert('10個までです。');
+            return false;
+        }
+		$(rootID).append('<div><span>'+$(this).val()+'</span><input type="checkbox" value="'+childrenNumber+'" /></div>');
+	}
+});
 
-const  book : book = {
-    title: '若者の全て',
-    price: 9999,
-}
-
-
-function say(elem: HTMLElement|null,bookname: string, price: number) {
-    if(elem !== null) {
-        elem.innerHTML = '書名：' + bookname + '  価格: ' + price + '円';
-    }
-}
-
-say(elem,book.title,book.price);
-
-
-
-// aBook.say(elem);
