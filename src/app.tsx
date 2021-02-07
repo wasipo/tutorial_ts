1a3379f9854fd454146d28bcd18435b464901c1e
horiguchi-note <horiguchiko@gmail.com>
2021-02-07 16:42:58 +0900

    静的なデータを追加する

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,52 @@
 import React from 'react'
 import ReactDOM from "react-dom";
-// import {Item} from './item';
-// import $ from 'jquery';
 
 
-class App extends React.Component {
+class App extends React.Component<{},{ listdata: { name: string; age: number; }[]; }> {
 
+    constructor(props: {} | Readonly<{}> ) {
+        super(props);
+        this.state = {
+            listdata: [
+                { name: 'haaa',age: 14},
+                { name: 'haawa',age: 18}
+            ]
+        };
+        this.renderRows = this.renderRows.bind(this)
+        this.addDatas = this.addDatas.bind(this)
+
+    }
+    addDatas() {
+
+        const newEntry = {name: 'kenta', age: 65}
+        const listdata = [
+            ...this.state.listdata,
+            newEntry
+        ]
+        this.setState({listdata});
+    }
+
+    renderRows() {
+        const {listdata} = this.state;
+        let result:any[] = [];
+        listdata.map(entry =>{
+            result.push(
+                <div>
+                    <div><span>{entry.name}</span><span>{entry.age}</span></div>
+                </div>
+            );
+        });
+        return result;
+    }
     render(){
         return (
-            <h1>Welcome!</h1>
-            );
+            <div>
+                <div>
+                    {this.renderRows()}
+                </div>
+                <button onClick={this.addDatas}>データ追加</button>
+            </div>
+        );
     }
 }
 
