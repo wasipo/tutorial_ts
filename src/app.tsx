411231b90d74cb0777501b195705f28b7e57e302
wasipo <horiguchiko@gmail.com>
2021-02-16 21:44:36 +0900

    定義したデータに応じてフォームを生成するコンポーネント作成

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from "react-dom";
 import Header from './components/header'
 import AddData from './functions/addData'
+import { BrowserRouter as Router, Route,Switch,MemoryRouter } from 'react-router-dom';
+import ReserveSetting from './ReserveSetting';
+
 
 class App extends React.Component<{},{ listdata: { name: string; age: number; }[]; }> {
     render(){
         return (
+            <MemoryRouter>
             <div>
                 <Header />
-                {/* <AddData /> */}
+                <Route path='/ReserveSetting' component={ReserveSetting}/>
             </div>
+            </MemoryRouter>
         );
     }
 }
--- src/components/form/ReserveSettingForm.tsx
+++ src/components/form/ReserveSettingForm.tsx
@@ -3,7 +3,7 @@ import ReactDom from "react-dom";
 import CreateForm from "../../parts/CreateForm"
 
 
-interface formData {
+type formData =  {
     id: number;
     formtype: string; 
     name: string|undefined;
@@ -12,55 +12,67 @@ interface formData {
     selected: string | null;
 }
 
+type formDataWrap = {
+    wrap : formData[]
+}
 
 
-class SettingForm extends React.Component<{},{listdata: formData[];}> {
+class SettingForm extends React.Component<{},{listdata: formDataWrap[];}> {
 
     constructor(props: {} | Readonly<{}>) {
         super(props);
         this.state = {
             listdata: [
-                { 
-                    id: 1,
-                    formtype: 'text',
-                    name: 'hogeyamada',
-                    formvalue: undefined,
-                    disabled: undefined,
-                    selected: null
-                },
-                { 
-                    id: 2,
-                    formtype: 'text', 
-                    name: 'iiyama',
-                    formvalue: undefined,
-                    disabled: undefined,
-                    selected: null
-                },
-                { 
-                    id: 3,
-                    formtype: 'text',
-                    name: 'tanaka',
-                    formvalue: undefined,
-                    disabled: undefined,
-                    selected: null
-                }
-            ]
+                {
+                    wrap :[
+                    { 
+                        id: 1,
+                        formtype: 'text',
+                        name: 'hogeyamada',
+                        formvalue: undefined,
+                        disabled: undefined,
+                        selected: null
+                    },
+                    { 
+                        id: 2,
+                        formtype: 'text', 
+                        name: 'iiyama',
+                        formvalue: undefined,
+                        disabled: undefined,
+                        selected: null
+                    }
+                ],
+            },
+            {
+                wrap :[
+                    { 
+                        id: 1,
+                        formtype: 'text',
+                        name: 'hogeyamada',
+                        formvalue: undefined,
+                        disabled: undefined,
+                        selected: null
+                    },
+                    { 
+                        id: 2,
+                        formtype: 'text', 
+                        name: 'iiyama',
+                        formvalue: undefined,
+                        disabled: undefined,
+                        selected: null
+                    }
+                ]
+            }
+            ],
         };
         this.renderForms.bind(this);
     }
     renderForms() {
         const {listdata} = this.state;
         let result:Array<JSX.Element | string | null > = [];
-        listdata.map((entry:formData,index) =>{
+        listdata.map((entry:formDataWrap,index) =>{
             result.push(
-                <input key={entry.id} name={entry.name} type={entry.formtype} value={entry.formvalue} />
-                // <CreateForm 
-                // id={entry.id}
-                // formtype={entry.formtype}
-                // name={entry.name} 
-                // formvalue={entry.formvalue} 
-                // disabled={entry.disabled} 
-                // selected={entry.selected} />
+                <input key={entry.wrap[index].id} name={entry.wrap[index].name} type={entry.wrap[index].formtype} value={entry.wrap[index].formvalue} />
             );
         });
 
@@ -71,9 +83,11 @@ class SettingForm extends React.Component<{},{listdata: formData[];}> {
     
     render () {
 
-        return (<div>
-            {this.renderForms()} 
-        </div>)
+        return (<form>
+            <div>
+                {this.renderForms()} 
+            </div>
+        </form>)
     } 
 
 
